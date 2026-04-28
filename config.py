9da1e19d401da6da832d8cca57d222d1abc9d6fa
sanjunewbot <lavotatunaxa79@gmail.com>
2026-04-28 09:24:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+ADMINS = list(map(int, os.getenv("ADMINS", "").split()))
